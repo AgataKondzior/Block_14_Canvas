8a47de49bbc6e15094494eaf5dcd62546a6cba22
AgataKondzior <ak.kondzior@gmail.com>
2017-04-12 16:07:07 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -4,5 +4,10 @@ var imange = new Image();
 image.onload = function() {
   console.log ("Loaded image");
   ctx.drawImage (image, 0,0, c.width, c.height);
+  var savedImage ="c.ToDataURL();
+  window.open(savedImage);
 }
 img.src = "#"
+
+fillRect(100,100,100,100);
+strokeRect(50,50,50,50);
