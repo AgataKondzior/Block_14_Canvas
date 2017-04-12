fdbede8e740f32b402ee3435bc5a44e937b829de
AgataKondzior <ak.kondzior@gmail.com>
2017-04-12 14:52:40 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,8 @@
-
+var c = getElementById("c");
+var ctx = c.getContext("2d");
+var imange = new Image();
+image.onload = function() {
+  console.log ("Loaded image");
+  ctx.drawImage (image, 0,0, c.width, c.height);
+}
+img.src = "#"
