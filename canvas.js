8933cb6e5f5b56c5a881140e09ac8f1c9d8b0c5d
AgataKondzior <ak.kondzior@gmail.com>
2017-04-13 14:15:32 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -6,32 +6,33 @@ if (id = "topLineText") {
     window.topLineText=text;
     
   } else {
-    window.bottomLineText=text;
-    
-    redrawMeme(window.imgSrc, window.topLineText, window.bottomLinetext);
-    
+    window.bottomLineText=text;   
+    redrawMeme(window.imgSrc, window.topLineText, window.bottomLinetext);    
   }
   
 function redrawMeme (image, topLine, bottomLine) {
     var canvas = (document).getElementByTagName("canvas");
     var ctx = canvas.getContext("2d");   
+  if(image ! = null);
+    ctx.drawImage (image, 0,0, canvas.width, canvas.height);
   
-  var imange = new Image();
-  image.onload = function() {
-  console.log ("Loaded image");
-  ctx.drawImage (image, 0,0, canvas.width, canvas.height);
-  var savedImage ="canvas.ToDataURL();
-  window.open(savedImage);
-}
-  img.src = "Meme.jpg";
-  
-  ctx.strokeStyle = "black";
-  ctx.strokeText (HELLO THERE, 50, 10);
   ctx.font = " 36px Arial";
-  ctx.fillText = "white";
-  ctx.fill();
-  }
+  ctx.textAlign = "center";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 3;
+  ctx.fillStyle = "white";
   
+ if  (topLine !=null) {
+   ctx.fillText(topLine, canvas.width/ 2,40;
+   ctx.strokeText(topLine, canvas.width/ 2,40;
+ }
+ 
+ if  (bottomLine !=null) {
+   ctx.fillText(bottomLine, canvas.width/ 2, canvas.height -20);
+   ctx.strokeText(bottomLine, canvas.width/ 2,canvas.height -20);
+ }
+}                                 
+                 
 function saveFile () {
     window.open(document).getElementByTagName("canvas").toDataURL());  
   }
