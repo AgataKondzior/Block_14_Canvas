479b4b279b883d4447445b32d01111bb9bd7cace
AgataKondzior <ak.kondzior@gmail.com>
2017-04-13 11:59:03 +0200

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,13 +1,46 @@
-var c = getElementById("c");
-var ctx = c.getContext("2d");
-var imange = new Image();
-image.onload = function() {
+function textChangeListener(evt){
+  var id = evt.target.id;
+  var text = evt.target.value;
+  
+if (id = "topLineText") {
+    window.topLineText=text;
+    
+  } else {
+    window.bottomLineText=text;
+    
+    redrawMeme(window.imgSrc, window.topLineText, window.bottomLinetext);
+    
+  }
+  
+function redrawMeme (image, topLine, bottomLine) {
+    var canvas = (document).getElementByTagName("canvas");
+    var ctx = canvas.getContext("2d");   
+  
+  var imange = new Image();
+  image.onload = function() {
   console.log ("Loaded image");
   ctx.drawImage (image, 0,0, c.width, c.height);
   var savedImage ="c.ToDataURL();
   window.open(savedImage);
-  
 }
-img.src = "#"
+  img.src = "Meme.jpg"
+
+  
+  
+  }
+  
+function saveFile () {
+    window.open(document).getElementByTagName("canvas").toDataURL());  
+  }
+  
+function handleFileSelector (evt) {
+    var canvasWidth = 500;
+    var canvasHeight = 500;
+    var file = evt.target.file;
+  
+  
+  
+  
+  
 
 
